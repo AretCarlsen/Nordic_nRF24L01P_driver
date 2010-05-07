418e4d907461d771a819ca23c647565845fc3f61
Aret N Carlsen <AretCarlsen@autonomoustools.com>
2010-05-06 23:42:18 -0500

    Now compiling.

--- Nordic_nRF24L01P.hpp
+++ Nordic_nRF24L01P.hpp
@@ -30,20 +30,27 @@ public:
 
   Nordic_nRF24L01P(AVROutputPin &new_CSN_pin, AVROutputPin &new_CE_pin)
   : CSN_pin(new_CSN_pin), CE_pin(new_CE_pin)
-  {
+  { }
+
+// Initialize
+  void init(bool power_up = true){
 // Initial pin states
     CE_pin.set_low();
     CSN_pin.set_high();
 
-    enable_features(true);
+    if(power_up){
+      set_power_state(true);
+      _delay_ms(3);
+    }
+
+    set_features_enabled(true);
   // Set an initial ARD of 500us.
     set_auto_retransmit_delay(2);
   }
 
 // Register
   typedef uint8_t Register_t;
-// Bit position and mask, where mask is usually _BV(BitPos)
-  typedef uint8_t BitPos_t;
+  typedef uint8_t Bit_t;
   typedef uint8_t BitMask_t;
 // SPI opcode
   typedef uint8_t Opcode_t;
@@ -138,17 +145,17 @@ public:
   }
 
   // Enable all features
-  inline void enable_features(bool new_value){
+  inline void set_features_enabled(bool new_value){
     // Enable dynamic payloads, ack payloads, and per-packet ack disabling
-    write_register_bits(Register_FEATURE, Bit_EN_DPL | Bit_EN_ACK_PAY | Bit_EN_DYN_ACK, new_value);
+    write_register_bits(Register_FEATURE, _BV(Bit_EN_DPL) | _BV(Bit_EN_ACK_PAY) | _BV(Bit_EN_DYN_ACK), new_value);
   }
   // True = mode TX, false = mode RX.
   inline void set_mode(Mode_t new_value){
-    write_register_bits(Register_CONFIG, Bit_PRIM_RX, new_value == Mode_RX);
+    write_register_bits(Register_CONFIG, _BV(Bit_PRIM_RX), new_value == Mode_RX);
   }
   // True = powered up, False = powered down.
   inline void set_power_state(bool new_value){
-    write_register_bits(Register_CONFIG, Bit_PWR_UP, new_value);
+    write_register_bits(Register_CONFIG, _BV(Bit_PWR_UP), new_value);
   }
   // True = chip enabled (CE high); false = chip disabled (CE low).
   inline void set_chip_enabled(bool new_value){
@@ -157,55 +164,55 @@ public:
   // Enable/disable which interrupts show up on the IRQ pin. (True = enabled, false = masked.)
   inline void set_interrupt_pin(Interrupt_t interrupt, bool new_value){
     // Inverted, because bit actually indicates masked interrupt.
-    write_register_bits(Register_CONFIG, Bit_MAX_RT << interrupt, ~new_value);
+    write_register_bits(Register_CONFIG, _BV(Bit_MAX_RT) << interrupt, ~new_value);
   }
   // Read an interrupt.
   inline bool read_interrupt(Interrupt_t interrupt){
-    return read_status() & (Bit_MAX_RT << interrupt);
+    return read_status() & (_BV(Bit_MAX_RT) << interrupt);
   }
   // Clear an interrupt
   inline void clear_interrupt(Interrupt_t interrupt){
     // Interrupts are cleared by writing a 1 to their flags the STATUS register.
-    write_register_OR(Register_STATUS, Bit_MAX_RT << interrupt); 
+    write_register_OR(Register_STATUS, _BV(Bit_MAX_RT) << interrupt); 
   }
   // Clear all three interrupts.
   inline void clear_all_interrupts(){
-    write_register_bits(Register_STATUS, Bit_MASK_RX_DR | Bit_MASK_TX_DS | Bit_MASK_MAX_RT, true);
+    write_register_bits(Register_STATUS, _BV(Bit_MASK_RX_DR) | _BV(Bit_MASK_TX_DS) | _BV(Bit_MASK_MAX_RT), true);
   }
   // Check interrupts
   inline bool check_packet_transmitted(){
     return read_interrupt(Interrupt_TX_DS);
   }
-  inline bool clear_packet_transmitted(){
-    return clear_interrupt(Interrupt_TX_DS);
+  inline void clear_packet_transmitted(){
+    clear_interrupt(Interrupt_TX_DS);
   }
   inline bool check_maximum_retries_reached(){
     return read_interrupt(Interrupt_MAX_RT);
   }
-  inline bool clear_maximum_retries_reached(){
-    return clear_interrupt(Interrupt_MAX_RT);
+  inline void clear_maximum_retries_reached(){
+    clear_interrupt(Interrupt_MAX_RT);
   }
   inline bool check_packet_received(){
     return read_interrupt(Interrupt_RX_DR);
   }
-  inline bool clear_packet_received(){
-    return clear_interrupt(Interrupt_RX_DR);
+  inline void clear_packet_received(){
+    clear_interrupt(Interrupt_RX_DR);
   }
 
 /* PACKET CONTROL */
   // Set the number of transmitted CRC bytes.
   // Note that a value of 0 will be clamped to 1 if AutoAcknowledge is enabled.
-  inline void set_CRC_size(const uint8_t new_value){
+  inline void set_CRC_size(uint8_t new_value){
   // Clamp to limits
     if(new_value > CRC_Max) new_value = CRC_Max;
 
     // Read config, clearing relevant flags.
-    uint8_t config = read_register(Register_CONFIG) & ~(Bit_EN_CRC | Bit_CRC0);
+    uint8_t config = read_register(Register_CONFIG) & ~(_BV(Bit_EN_CRC) | _BV(Bit_CRCO));
     // For CRC size 0, no change
-    // For CRC size 1, EN_CRC=1, CRC0=0
-    if(new_value == 1) config |= Bit_EN_CRC;
-    // For CRC size 2, EN_CRC=CRC0=1
-    else if(new_value == 2) config |= Bit_EN_CRC | Bit_CRC0;
+    // For CRC size 1, EN_CRC=1, CRCO=0
+    if(new_value == 1) config |= _BV(Bit_EN_CRC);
+    // For CRC size 2, EN_CRC=CRCO=1
+    else if(new_value == 2) config |= _BV(Bit_EN_CRC) | _BV(Bit_CRCO);
 
     write_register(Register_CONFIG, config);
   }
@@ -218,11 +225,11 @@ public:
   }
   // Activate dynamic payload size for a pipe.
   inline void set_dynamic_payload_allowed(const Pipe_t pipe, bool new_value){
-    write_register_bits(Register_DYNPD, Bit_DPL_P5, new_value);
+    write_register_bits(Register_DYNPD, _BV(Bit_DPL_P5), new_value);
   }
   // Activate all dynamic payloads
   inline void set_all_dynamic_payloads_allowed(bool new_value){
-    write_register_bits(Register_DYNPD, Bit_DPL_P5 | DPL_P4 | DPL_P3 | DPL_P2 | DPL_P1 | DPL_P0, new_value);
+    write_register_bits(Register_DYNPD, _BV(Bit_DPL_P5) | _BV(Bit_DPL_P4) | _BV(Bit_DPL_P3) | _BV(Bit_DPL_P2) | _BV(Bit_DPL_P1) | _BV(Bit_DPL_P0), new_value);
   }
   // Enable/disable auto ACK for a pipe.
     // Note that auto ACK is incompatible with the nRF24L01 (without the +).
@@ -231,26 +238,26 @@ public:
     if(pipe > Pipe_Max) return;
   // Could use a switch statement to pick up the individual definitions.
   // However, they are all lined up in the register anyway, and the sanity check already happened.
-    write_register_bits(Register_EN_AA, Bit_ENAA_P0 << pipe, new_value);
+    write_register_bits(Register_EN_AA, _BV(Bit_ENAA_P0) << pipe, new_value);
   }
   // Set the auto-retransmit delay (ARD), in 250us units. This should be at least 500us in many situations.
   inline void set_auto_retransmit_delay(ARD_t new_delay){
     if(new_delay > AutoRetransmitDelay_Max) new_delay = AutoRetransmitDelay_Max;
-    write_register(Register_SETUP_RETR, (read_register(Register_SETUP_RETR) & ~BitMask_ARD) | (new_delay << BitPos_ARD));
+    write_register(Register_SETUP_RETR, (read_register(Register_SETUP_RETR) & ~BitMask_ARD) | (new_delay << Bit_ARD));
   }
   // Set the maximum auto-retransmit count. 0 => disabled.
   inline void set_auto_retransmit_count_limit(uint8_t new_limit){
     // Clamp to limits
     if(new_limit > AutoRetransmitCount_Max) new_limit = AutoRetransmitCount_Max;
-    write_register(Register_SETUP_RETR, (read_register(Register_SETUP_RETR) & ~Bitmask_ARC) | (new_limit << BitPos_ARC));
+    write_register(Register_SETUP_RETR, (read_register(Register_SETUP_RETR) & ~BitMask_ARC) | (new_limit << Bit_ARC));
   }
   // Persists until RF channel is changed.
   inline uint8_t read_lost_packet_count(){
-     return (read_register(Register_OBSERVE_TX) & BitMask_PLOS_CNT) >> BitPos_PLOS_CNT;
+     return (read_register(Register_OBSERVE_TX) & BitMask_PLOS_CNT) >> Bit_PLOS_CNT;
   }
   // Resets on every new packet transmission.
   inline uint8_t read_retransmission_count(){
-     return (read_register(Register_OBSERVE_TX) & BitMask_ARC_CNT) >> BitPos_ARC_CNT;
+     return (read_register(Register_OBSERVE_TX) & BitMask_ARC_CNT) >> Bit_ARC_CNT;
   }
 
 /* RF CONTROL */
@@ -265,10 +272,10 @@ public:
   // Set the RF send/receive frequency, in MHz.
   inline void set_frequency(Frequency_t new_frequency){
     // Clamp to limits.
-    if(new_frequency < Min_Frequency) new_frequency = Min_Frequency;
-    else if(new_frequency > Max_Frequency) new_frequency = Max_Frequency;
-  // Channel is MHz above Min_Frequency (2400MHz). For instance, 2405MHz is channel 5.
-    set_channel(new_frequency - Min_Frequency);
+    if(new_frequency < Frequency_Min) new_frequency = Frequency_Min;
+    else if(new_frequency > Frequency_Max) new_frequency = Frequency_Max;
+  // Channel is MHz above Frequency_Min (2400MHz). For instance, 2405MHz is channel 5.
+    set_channel(new_frequency - Frequency_Min);
   }
   // Set the RF gain. Provided value is rounded down to next lowest allowed value.
   inline void set_gain(Gain_t new_gain){
@@ -277,16 +284,16 @@ public:
     else if(new_gain > Gain_Max) new_gain = Gain_Max;
 
   // Translate into nonnegative integer space and scale.
-    write_register(Register_RF_SETUP, ((new_gain - MIN_GAIN) / 6) << BitPos_RF_PWR);
+    write_register(Register_RF_SETUP, ((new_gain - Gain_Min) / 6) << Bit_RF_PWR);
   }
   // Set the data rate to one of the allowed values.
   inline void set_data_rate(Rate_t new_rate){
   // Clear both bits.
-    uint8_t rf_setup = read_register(RF_SETUP) & ~(Bit_RF_DR_HIGH | Bit_RF_DR_LOW);
+    uint8_t rf_setup = read_register(Register_RF_SETUP) & ~(_BV(Bit_RF_DR_HIGH) | _BV(Bit_RF_DR_LOW));
   // For rate 250K, set DR_LOW.
-    if(new_rate == Rate_250K) rf_setup |= Bit_RF_DR_LOW;
+    if(new_rate == Rate_250K) rf_setup |= _BV(Bit_RF_DR_LOW);
   // For rate 2M, set DR_HIGH.
-    else if(new_rate == Rate_2M) rf_setup |= Bit_RF_DR_HIGH;
+    else if(new_rate == Rate_2M) rf_setup |= _BV(Bit_RF_DR_HIGH);
   // For rate 1M, leave both bits cleared.
 
   // Write out config.
@@ -306,7 +313,7 @@ public:
   // Register contains address size, offset by 1.
     write_register(Register_SETUP_AW, new_size + 1);
   }
-  inline void write_address(const uint8_t reg, uint8_t* new_address, const uint8_t len){
+  inline void write_address(const uint8_t reg, uint8_t* new_address, uint8_t len){
   // Trim size to maximum address size.
     if(len > AddressSize_Max) len = AddressSize_Max; // new_address += AddressSize_Max - len;
 
@@ -317,7 +324,7 @@ public:
     uint8_t i = 0;
   // Write the actual data
     for(; i < len; i++){
-      SPI::transceive(new_address);
+      SPI::transceive(*new_address);
       new_address++;
     }
   // Write the zero'd MSBs, if any
@@ -334,13 +341,13 @@ public:
   // Set ACK receive pipe to matching address, if desired.
     if(set_matching_ACK){
       set_RX_address(ACK_Rx_Pipe, new_address, len);
-      set_RX_pipe_enabled(ACK_Rx_Pipe);
+      set_RX_pipe_enabled(ACK_Rx_Pipe, true);
     }
   }
   // Set transmission address from a 32-bit unsigned int, rather than an array.
   inline void set_TX_address(const uint32_t &new_address, bool set_matching_ACK = true){
   // Presumes little-endian integer storage.
-    set_TX_address(&new_address, sizeof(uint32_t), set_matching_ACK);
+    set_TX_address((uint8_t*) &new_address, sizeof(uint32_t), set_matching_ACK);
   }
   // Set the receive address for a pipe from an array of bytes.
   inline void set_RX_address(uint8_t pipe, uint8_t* const new_address, const uint8_t len){
@@ -353,41 +360,39 @@ public:
   // Set receive address from a 32-bit unsigned int, rather than an array.
   inline void set_RX_address(uint8_t pipe, const uint32_t &new_address){
   // Presumes little-endian integer storage.
-    set_RX_address(pipe, &new_address, sizeof(uint32_t));
+    set_RX_address(pipe, (uint8_t*) &new_address, sizeof(uint32_t));
   }
   inline void set_RX_pipe_enabled(uint8_t pipe, bool new_value){
   // Sanity check
     if(pipe > Pipe_Max) return;
   // Could use a switch statement to pick up the individual definitions.
   // However, they are all lined up in the register anyway, and the sanity check already happened.
-    write_register_bits(Register_EN_RXADDR, ERX_P0 << pipe, new_value);
+    write_register_bits(Register_EN_RXADDR, Bit_ERX_P0 << pipe, new_value);
   }
 
 /* FIFO CONTROL */
-  // In TX mode, add a packet to the outgoing queue. The argument determines whether an ACK is requested.
-  // In RX mode, add a packet to the ACK-payload outgoing queue. (Ack_requested argument is ignored.)
-  inline void queue_TX_packet(const uint8_t* buf, const uint8_t buf_len, bool ack_requested = true){
-    if(buf_len > PacketSize_Max) buf_len = PacketSize_Max;
-
+  inline void queue_packet(const uint8_t opcode, const uint8_t *buf, uint8_t buf_len){
     CSN_pin.set_low();
-  // Opcode depends on ACK preference.
-    SPI::transceive(ack_requested? Opcode_W_TX_PAYLOAD : W_TX_PAYLOAD_NOACK);
+    SPI::transceive(opcode);
   // Send packet payload.
-    SPI::transceive(buf, buf_len); 
+    SPI::transmit(buf, buf_len);
     CSN_pin.set_high();
   }
+  // In TX mode, add a packet to the outgoing queue. The argument determines whether an ACK is requested.
+  // In RX mode, add a packet to the ACK-payload outgoing queue. (Ack_requested argument is ignored.)
+  inline void queue_TX_packet(const uint8_t* buf, uint8_t buf_len, bool ack_requested = true){
+    if(buf_len > PacketSize_Max) buf_len = PacketSize_Max;
+
+  // Opcode depends on ACK preference.
+    queue_packet(ack_requested? Opcode_W_TX_PAYLOAD : Opcode_W_TX_PAYLOAD_NOACK, buf, buf_len);
+  }
   // Queue an outgoing RX packet (ACK payload), associated with a specific address/pipe.
-  inline void queue_RX_packet(const uint8_t* buf, const uint8_t buf_len, const Pipe_t rx_pipe){
-    // Ignore if RX pipe out of bounds
+  inline void queue_RX_packet(const uint8_t* buf, uint8_t buf_len, const Pipe_t rx_pipe){
+    // Reject if RX pipe out of bounds
     if(rx_pipe > Pipe_Max) return;
     if(buf_len > PacketSize_Max) buf_len = PacketSize_Max;
 
-    CSN_pin.set_low();
-  // Specify pipe with opcode.
-    SPI::transceive(Opcode_W_ACK_PAYLOAD | rx_pipe);
-  // Send packet payload.
-    SPI::transceive(buf, buf_len);
-    CSN_pin.set_high();
+    queue_packet(Opcode_W_ACK_PAYLOAD | rx_pipe, buf, buf_len);
   }
   // Transmit a single packet. Does nothing if transmission is already in progress, in RX mode, or if
   // MIRF is in a constantly transmitting state.
@@ -431,7 +436,7 @@ public:
   // Receive packet.
     CSN_pin.set_low();
     SPI::transceive(Opcode_R_RX_PAYLOAD); 
-    SPI::transceive(buf, buf_len); 
+    SPI::receive(buf, buf_len); 
     CSN_pin.set_high();
 
     return packet_size;
@@ -444,11 +449,11 @@ public:
 
   // Check whether the transmission packet FIFO is full.
   inline bool transmit_buffer_full(){
-    return (read_status() & Bit_TX_FULL):
+    return (read_status() & _BV(Bit_STATUS_TX_FULL));
   }
   // Check whether the transmission packet FIFO is empty.
   inline bool transmit_buffer_empty(){
-    return (read_register(Register_FIFO_STATUS) & Bit_FIFO_TX_FULL);
+    return (read_register(Register_FIFO_STATUS) & _BV(Bit_FIFO_TX_FULL));
   }
   // Flush the transmission FIFO.
   inline void flush_transmit_buffer(){
@@ -459,17 +464,17 @@ public:
 
   // Check whether the reception packet FIFO is full.
   inline bool receive_buffer_full(){
-    return (read_register(Register_FIFO_STATUS) & Bit_RX_FULL);
+    return (read_register(Register_FIFO_STATUS) & _BV(Bit_RX_FULL));
   }
   // Check which RX pipe a received packet (at the front of the FIFO) is
   // associated with. Returns a nonsensical value (7) if no packet
   // is pending.
   inline uint8_t read_received_packet_pipe(){
-    return (read_status() & BitMask_RX_P_NO) >> BitPos_RX_P_NO;
+    return (read_status() & BitMask_RX_P_NO) >> Bit_RX_P_NO;
   }
   // Check whether the reception packet FIFO is empty.
   inline bool receive_buffer_empty(){
-    return ((read_status() & BitMask_RX_P_NO) == BitMask_RX_P_NO_Empty);   // read_register(Register_FIFO_STATUS) & Bit_RX_EMPTY
+    return ((read_status() & BitMask_RX_P_NO) == BitMask_RX_P_NO_Empty);   // read_register(Register_FIFO_STATUS) & _BV(Bit_RX_EMPTY)
   }
   // Flush the reception FIFO. Note that this may corrupt any ACKs in progress.
   inline void flush_receive_buffer(){
